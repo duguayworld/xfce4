a8b48c51a3f08e3893660704a3eae4daa01b7e49
Benjamin Duguay <153779837+duguayworld@users.noreply.github.com>
2024-06-10 11:01:27 -0400

    Create colors.lua

---
+++ lite-xl/colors.lua
@@ -0,0 +1,28 @@
+local style = require "core.style"
+local common = require "core.common"
+
+style.background = { common.color "#0C0A16" }
+style.background2 = { common.color "#22242e" }
+style.background3 = { common.color "#312437" }
+style.text = { common.color "#d7c0cf" }
+style.caret = { common.color "#a93f2e" }
+style.accent = { common.color "#f5faff" }
+style.dim = { common.color "#4f526b" }
+style.divider = { common.color "#22242e" }
+style.selection = { common.color "#4f526b" }
+style.line_number = { common.color "#312434" }
+style.line_number2 = { common.color "#ff79c6" }
+style.line_highlight = { common.color "#4f526b" }
+style.scrollbar = { common.color "#0C0A16" }
+style.scrollbar2 = { common.color "#312434" }
+
+style.syntax["normal"] = { common.color "#f5deed" }
+style.syntax["symbol"] = { common.color "#f5faff" }
+style.syntax["comment"] = { common.color "#b0b0b0" }
+style.syntax["keyword"] = { common.color "#edc8f9" }
+style.syntax["keyword2"] = { common.color "#bd93f9" }
+style.syntax["number"] = { common.color "#ff79c6" }
+style.syntax["literal"] = { common.color "#bd93f9" }
+style.syntax["string"] = { common.color "#8CD0D3" }
+style.syntax["operator"] = { common.color "#ff79c6" }
+style.syntax["function"] = { common.color "#8be9fd" }
